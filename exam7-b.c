0396cd189124de2f57363035b4da0058e12c7c5f
Shubarina01gr <91784691+Shubarina01gr@users.noreply.github.com>
2021-11-26 22:02:07 +0300

    Create exam7-b.c

---
+++ exam7-b.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+
+float av(float x, float z)
+{
+ float y;
+ y = x / z;
+ return(y);
+}
+
+int main()
+{
+    int N;
+    printf("Введите количество оценок: ");
+    scanf("%d", &N);
+
+    int a[N];
+    float sum;
+   
+    
+
+    printf("Введите оценки: ");
+    for(int i = 0; i < N; i++)
+    {
+     scanf("%d", &a[i]);
+
+     if (a[i] > 5 || a[i] < 2)
+     {
+      printf("Некорректно введены данные");   
+      return 0;
+     }
+     sum += a[i];
+    }
+
+    float avg = av(sum, N);
+    printf ("Средний балл ученика: %.1f\n",avg);
+
+    if(avg >= 4.6)
+    printf("Ученик сможет поступить в данный универ\n");
+    else
+    printf("Ученик не сможет поступить в данный универ\n");
+
+    return 0;
+}
